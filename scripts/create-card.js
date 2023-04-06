83bc8fbbf0c69296445232362b7621025076ef92
Quilo <quilo.bikcodes@gmail.com>
2023-04-07 03:50:24 +0530

    Initialize Repo

--- scripts/create-card.js
+++ scripts/create-card.js
@@ -1,17 +1,38 @@
 console.log("Fetch Data")
 const subjectBox = document.querySelector("#subject-input")
+const teacherBox = document.querySelector("#teacher-input")
 const addBtn = document.querySelector("#add-btn-id")
 const cards = document.querySelector("#cards")
 
 addBtn.addEventListener("click",()=>{
     var subjecttxt = subjectBox.value
-    console.log(subjecttxt)
+    var teachertxt = teacherBox.value
 
-    const card = document.createElement("div")
-    const text = document.createTextNode(subjecttxt)
+    if(subjecttxt === '' && subjecttxt === null){
+        alert("Please fill your subject atleast")
+    }
+    if(subjecttxt.length>=20 && teachertxt.length>=20){
+        alert("Details must be shorter than 20 characters")    
+    }
+    else if(subjecttxt.length<=3 && teachertxt.length<=3){
+        alert("Details must be longer than 3 characters")    
+    }
+    else{
+        console.log(subjecttxt)
+        const card = document.createElement("div")
+        const subjectcard = document.createElement("span")
+        const teachercard = document.createElement('span') 
+        subjectcard.innerText = subjecttxt
+        teachercard.innerText = teachertxt
+    
+    
+    
+        card.classList.add("card")
+        subjectcard.setAttribute('id', 'subjects');
+        teachercard.setAttribute('id', 'subject-teachers');
+        card.appendChild(subjectcard)
+        card.appendChild(teachercard)
+        cards.append(card)
+    }
 
-    card.classList.add("class")
-
-    card.appendChild(text)
-    cards.append(card)
 })
--- scripts/design-maintenance.js
+++ scripts/design-maintenance.js
@@ -1,1 +1,7 @@
 console.log("dm")
+const video = document.querySelector('#video-background')
+const proceedbtn = document.querySelector('#proceed-btn')
+
+proceedbtn.addEventListener('click',()=>{
+    video.play()
+})
