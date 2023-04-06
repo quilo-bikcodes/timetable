cbc723cc9447bd4052c2b78ca6798a8b39efcc96
quilo-bikcodes <quilo.bikcodes@gmail.com>
2023-04-07 04:56:11 +0530

    complee

--- scripts/create-card.js
+++ scripts/create-card.js
@@ -1,38 +1,38 @@
-console.log("Fetch Data")
-const subjectBox = document.querySelector("#subject-input")
-const teacherBox = document.querySelector("#teacher-input")
-const addBtn = document.querySelector("#add-btn-id")
-const cards = document.querySelector("#cards")
+console.log("Fetch Data");
+const subjectBox = document.querySelector("#subject-input");
+const teacherBox = document.querySelector("#teacher-input");
+const addBtn = document.querySelector("#add-btn-id");
+const cards = document.querySelector("#cards");
 
-addBtn.addEventListener("click",()=>{
-    var subjecttxt = subjectBox.value
-    var teachertxt = teacherBox.value
+addBtn.addEventListener("click", () => {
+  var subjecttxt = subjectBox.value;
+  var teachertxt = teacherBox.value;
 
-    if(subjecttxt === '' && subjecttxt === null){
-        alert("Please fill your subject atleast")
-    }
-    if(subjecttxt.length>=20 && teachertxt.length>=20){
-        alert("Details must be shorter than 20 characters")    
-    }
-    else if(subjecttxt.length<=3 && teachertxt.length<=3){
-        alert("Details must be longer than 3 characters")    
-    }
-    else{
-        console.log(subjecttxt)
-        const card = document.createElement("div")
-        const subjectcard = document.createElement("span")
-        const teachercard = document.createElement('span') 
-        subjectcard.innerText = subjecttxt
-        teachercard.innerText = teachertxt
-    
-    
-    
-        card.classList.add("card")
-        subjectcard.setAttribute('id', 'subjects');
-        teachercard.setAttribute('id', 'subject-teachers');
-        card.appendChild(subjectcard)
-        card.appendChild(teachercard)
-        cards.append(card)
-    }
+  if (subjecttxt === "" && subjecttxt === null) {
+    alert("Please fill your subject atleast");
+  }
+  if (subjecttxt.length >= 20 && teachertxt.length >= 20) {
+    alert("Details must be shorter than 20 characters");
+  } else if (subjecttxt.length <= 3 && teachertxt.length <= 3) {
+    alert("Details must be longer than 3 characters");
+  } else {
+    console.log(subjecttxt);
+    const card = document.createElement("div");
+    const subjectcard = document.createElement("span");
+    const teachercard = document.createElement("span");
+    subjectcard.innerText = subjecttxt;
+    teachercard.innerText = teachertxt;
 
-})
+    card.classList.add("card");
+    subjectcard.setAttribute("id", "subjects");
+    teachercard.setAttribute("id", "subject-teachers");
+    card.appendChild(subjectcard);
+    card.appendChild(teachercard);
+    cards.append(card);
+  }
+});
+
+$(window).scroll(function() {
+  console.log("l")
+});
+
--- scripts/design-maintenance.js
+++ scripts/design-maintenance.js
@@ -1,6 +1,19 @@
 console.log("dm")
-const video = document.querySelector('#video-background')
+const video = document.querySelector('#video-background-mp4')
 const proceedbtn = document.querySelector('#proceed-btn')
+const nav = document.querySelector('#nav-login');
+
+// console.log(distanceFromTop);
+window.addEventListener('scroll', () =>{
+    console.log("l")
+    const distanceFromTop = window.pageYOffset - document.documentElement.scrollTop;
+    console.log(distanceFromTop);
+    if (distanceFromTop > 100) {
+      nav.classList.add('dark');
+    } else {
+      nav.classList.remove('dark');
+    }
+  });
 
 proceedbtn.addEventListener('click',()=>{
     video.play()
